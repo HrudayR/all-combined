b7721b76b403ac1976a5820648ec54cda66f849c
HrudayR <85166622+HrudayR@users.noreply.github.com>
2023-06-13 17:55:39 +0530

    Add files via upload

---
+++ Informer/mendeley.sh
@@ -0,0 +1,23 @@
+# VOLTAGE
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Voltage --seq_len 96 --label_len 48 --pred_len 24 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Voltage --seq_len 96 --label_len 48 --pred_len 48 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Voltage --seq_len 96 --label_len 48 --pred_len 96 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Voltage --seq_len 96 --label_len 48 --pred_len 168 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Voltage --seq_len 96 --label_len 48 --pred_len 264 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Voltage --seq_len 96 --label_len 48 --pred_len 312 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+
+# CURRENT
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Current --seq_len 96 --label_len 48 --pred_len 24 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Current --seq_len 96 --label_len 48 --pred_len 48 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Current --seq_len 96 --label_len 48 --pred_len 96 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Current --seq_len 96 --label_len 48 --pred_len 168 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Current --seq_len 96 --label_len 48 --pred_len 264 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Current --seq_len 96 --label_len 48 --pred_len 312 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+
+# CAPACITY
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Ah --seq_len 96 --label_len 48 --pred_len 24 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Ah --seq_len 96 --label_len 48 --pred_len 48 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Ah --seq_len 96 --label_len 48 --pred_len 96 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Ah --seq_len 96 --label_len 48 --pred_len 264 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Ah --seq_len 96 --label_len 48 --pred_len 168 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
+python3 -u main_informer.py --model informer --data mendeley --data_path mendeley.csv --features S --target Ah --seq_len 96 --label_len 48 --pred_len 312 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --train_epochs 10
